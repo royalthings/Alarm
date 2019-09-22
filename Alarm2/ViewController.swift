d386a5913098751405f03b4b6ef3e8a1556cab1b
Дмитрий Ага <royalthings2019@gmail.com>
2019-09-22 12:52:05 +0300

    Alarm2

--- Alarm2/ViewController.swift
+++ Alarm2/ViewController.swift
@@ -11,32 +11,37 @@ import AVFoundation
 
 class ViewController: UIViewController {
    
-   
+   var timer: Timer?
    @IBOutlet weak var stopButton: UIButton!
+   
+   var releaseDate: Date?
+   var countdownTimer = Timer()
    
    var soundEnable = true
    var audioPlayer: AVAudioPlayer?
    
    override func viewDidLoad() {
       super.viewDidLoad()
-
+      
       stopButton.isHidden = true
       playMusic()
-      currentDate()
+      
+      startTimer()
+
    }
    
-   func currentDate() {
-      let currentDate = Date()
-      let calendar = Calendar.current
-      let diffDateComponents = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate)
-      print(diffDateComponents.hour)
-      if diffDateComponents.hour == 4 {
-         audioPlayer?.volume = 1
-         stopButton.isHidden = false
-      } else if diffDateComponents.hour == 11 {
-         audioPlayer?.volume = 1
-         stopButton.isHidden = false
-      } else if diffDateComponents.hour == 12 {
+   func startTimer() {
+      timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+      
+   }
+   
+   @objc func updateTime() {
+      
+      let currentTime = DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .medium)
+      print(currentTime)
+      
+      
+      if currentTime == "10:00:00 AM" || currentTime == "11:00:00 AM" || currentTime == "12:00:00 AM" {
          audioPlayer?.volume = 1
          stopButton.isHidden = false
       }
@@ -58,7 +63,5 @@ class ViewController: UIViewController {
       audioPlayer?.volume = 0
       stopButton.isHidden = true
    }
-   
-
 }
 
